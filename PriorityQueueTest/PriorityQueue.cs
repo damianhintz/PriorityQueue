0b0db537ba327ce191785baedd41b57cfa4c083c
Damian Hintz <damian.hintz@gmail.com>
2017-05-04 20:56:43 +0200

    Add solution to git

---
+++ PriorityQueueTest/PriorityQueue.cs
@@ -0,0 +1,149 @@
+﻿using System.Threading;
+using System.Collections.Generic;
+using System;
+
+namespace PriorityQueueTest
+{
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        private static int _count = int.MinValue;
+        private IndexedItem[] _items;
+        private int _size;
+
+        public PriorityQueue()
+            : this(16)
+        {
+        }
+
+        public PriorityQueue(int capacity)
+        {
+            _items = new IndexedItem[capacity];
+            _size = 0;
+        }
+
+        public int Count { get { return _size; } }
+
+        public T Max()
+        {
+            if (_size == 0)
+                throw new InvalidOperationException("HEAP_EMPTY");
+
+            return _items[0].Value;
+        }
+
+        public T Dequeue()
+        {
+            var result = Max();
+            RemoveAt(0);
+            return result;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (_size >= _items.Length)
+            {
+                var temp = _items;
+                _items = new IndexedItem[_items.Length * 2];
+                Array.Copy(temp, _items, temp.Length);
+            }
+
+            var index = _size++;
+            _items[index] = new IndexedItem
+            {
+                Value = item,
+                Id = Interlocked.Increment(ref _count)
+            };
+            Percolate(index);
+        }
+
+        private bool IsHigherPriority(int left, int right)
+        {
+            return _items[left].CompareTo(_items[right]) > 0;
+        }
+
+        private void Percolate(int index)
+        {
+            if (index >= _size || index < 0)
+                return;
+            var parent = (index - 1) / 2;
+            if (parent < 0 || parent == index)
+                return;
+
+            if (IsHigherPriority(index, parent))
+            {
+                var temp = _items[index];
+                _items[index] = _items[parent];
+                _items[parent] = temp;
+                Percolate(parent);
+            }
+        }
+
+        private void Heapify()
+        {
+            Heapify(0);
+        }
+
+        private void Heapify(int index)
+        {
+            if (index >= _size || index < 0)
+                return;
+
+            var left = 2 * index + 1;
+            var right = 2 * index + 2;
+            var first = index;
+
+            if (left < _size && IsHigherPriority(left, first))
+                first = left;
+            if (right < _size && IsHigherPriority(right, first))
+                first = right;
+            if (first != index)
+            {
+                var temp = _items[index];
+                _items[index] = _items[first];
+                _items[first] = temp;
+                Heapify(first);
+            }
+        }
+
+        private void RemoveAt(int index)
+        {
+            _items[index] = _items[--_size];
+            _items[_size] = default(IndexedItem);
+            Heapify();
+            if (_size < _items.Length / 4)
+            {
+                var temp = _items;
+                _items = new IndexedItem[_items.Length / 2];
+                Array.Copy(temp, 0, _items, 0, _size);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            for (var i = 0; i < _size; ++i)
+            {
+                if (EqualityComparer<T>.Default.Equals(_items[i].Value, item))
+                {
+                    RemoveAt(i);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        struct IndexedItem : IComparable<IndexedItem>
+        {
+            public T Value;
+            public int Id;
+
+            public int CompareTo(IndexedItem other)
+            {
+                var c = Value.CompareTo(other.Value);
+                if (c == 0)
+                    c = Id.CompareTo(other.Id);
+                return c;
+            }
+        }
+    }
+}
---
+++ PriorityQueueTest/PriorityQueueTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C5;
+using Shouldly;
+
+namespace PriorityQueueTest
+{
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        [TestMethod]
+        public void IntervalHeap_ShouldBe()
+        {
+            var heap = new IntervalHeap<int>();
+
+            heap.AllowsDuplicates.ShouldBe(true);
+            heap.Count.ShouldBe(0);
+            heap.Add(1);
+            heap.Count.ShouldBe(1);
+            heap.FindMax().ShouldBe(1);
+            heap.Add(1);
+            heap.Count.ShouldBe(2);
+            heap.Add(10);
+            heap.FindMax().ShouldBe(10);
+            heap.FindMin().ShouldBe(1);
+            heap.Count.ShouldBe(3);
+            heap.Add(5);
+            heap.Count.ShouldBe(4);
+            heap.FindMax().ShouldBe(10);
+            heap.Add(100);
+            heap.FindMax().ShouldBe(100);
+            
+        }
+        
+        [TestMethod]
+        public void PriorityQueue_ShouldBe()
+        {
+            var heap = new PriorityQueue<int>();
+            
+            heap.Count.ShouldBe(0);
+
+            heap.Enqueue(5);
+            heap.Count.ShouldBe(1);
+            heap.Max().ShouldBe(5);
+
+            heap.Enqueue(10);
+            heap.Count.ShouldBe(2);
+            heap.Max().ShouldBe(10);
+
+            heap.Enqueue(5);
+            heap.Count.ShouldBe(3);
+            heap.Max().ShouldBe(10);
+
+            heap.Dequeue().ShouldBe(10);
+            heap.Count.ShouldBe(2);
+            heap.Max().ShouldBe(5);
+
+            heap.Enqueue(3);
+            heap.Max().ShouldBe(5);
+
+            heap.Dequeue().ShouldBe(5);
+            heap.Dequeue().ShouldBe(5);
+            heap.Dequeue().ShouldBe(3);
+            heap.Count.ShouldBe(0);
+
+            for(int i = 0; i < 100; i++)
+            {
+                heap.Enqueue(i);
+            }
+
+            for (int i = 99; i >= 0; i--)
+            {
+                heap.Dequeue().ShouldBe(i);
+            }
+
+            
+        }
+    }
+}
